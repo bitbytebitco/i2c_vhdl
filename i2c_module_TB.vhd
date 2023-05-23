5c65c3ed35816d921ef9c221b79bbdaa595fc0fe
bitbytebitco <45221627+bitbytebitco@users.noreply.github.com>
2023-05-23 15:47:38 -0600

    Update i2c_module_TB.vhd

--- i2c_module_TB.vhd
+++ i2c_module_TB.vhd
@@ -21,28 +21,22 @@ end entity;
 architecture i2c_module_TB_arch of i2c_module_TB is
 	-- Initializations
 	constant t_clk_per : time := 10 ns;  -- Period of a 100 MHz Clock
-	
---	type DataArray is array (natural range <>) of std_logic_vector(7 downto 0);
 
-	-- Component Declaration
-	component i2c_module 
-	    port(
-		i_RESET : in std_logic;
-		i_CLK : in std_logic;
---		i_start_flag : in std_logic;
---        i_addr : in std_logic_vector(7 downto 0);
---        i_data : in DataArray(0 to 2);
-		io_SCL : inout std_logic
---		io_SDA : inout std_logic
-	    );
-	end component;
+    component top 
+        port(
+            i_CLK : in std_logic;
+            i_RESET : in std_logic;
+            io_SCL : inout std_logic;
+            io_SDA : inout std_logic
+        );
+    end component;
 
 	-- Signal declarations
 	signal s_CLK_TB : std_logic;
 	signal s_sda_TB : std_logic;
 	signal s_scl_TB : std_logic;
 	signal s_reset_TB : std_logic;
-	signal test_addr : std_logic_vector(7 downto 0) := "11010000"; -- hard coded address of 0x68 w/ 0 for WR
+	signal test_addr : std_logic_vector(6 downto 0) := "1101000"; -- hard coded address of 0x68 w/ 0 for WR
     signal test_data : DataArray(0 to 3);
     
 	signal io_SDA_TB : std_logic;
@@ -54,15 +48,11 @@ architecture i2c_module_TB_arch of i2c_module_TB is
     test_data(2) <= "10100100";
     test_data(3) <= "00001110";
     
-	DUT : i2c_module 
-	    port map(
+	
+	DUT : top port map(
 		i_RESET => s_reset_TB,
---		i_start_flag => s_sda_TB,
---        i_addr => test_addr,
---        i_data => test_data,
-		io_SCL => s_scl_TB,
-		i_CLK => s_CLK_TB
---		io_SDA => io_SDA_TB
+        i_CLK => s_CLK_TB,
+		io_SCL => s_scl_TB
 	);
 
 	------------------------------------------------------------------
@@ -104,7 +94,7 @@ architecture i2c_module_TB_arch of i2c_module_TB is
         RESET_STIM : process
         begin
             s_reset_TB <= '1'; wait for 2*t_clk_per; 
-	       s_reset_TB <= '0'; wait for 2*t_clk_per; 
+	        s_reset_TB <= '0'; wait for 2*t_clk_per; 
             s_reset_TB <= '1'; wait; 
         end process;
 	----------------------------------------------- 
