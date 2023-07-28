7ffa6b87d3cf512f81986670ba9a1525f613109c
bitbytebitco <45221627+bitbytebitco@users.noreply.github.com>
2023-07-28 15:09:50 -0600

    Update i2c_module_write.vhd

--- i2c_module_write.vhd
+++ i2c_module_write.vhd
@@ -22,24 +22,26 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+--        i_addr : in std_logic_vector(6 downto 0);
+
 entity i2c_module_write is
     generic(
-        g_CLK_RATE : integer := 100_000_000
+        g_CLK_RATE : integer := 50_000_000
     );
     port(
         i_reset_n : in std_logic;				-- Active low reset
-        i_CLK : in std_logic;				-- ASSUMING: 100 MHz 
+        i_CLK : in std_logic;				
         i_en : in std_logic;
+        i_addr : in std_logic_vector(6 downto 0);
         i_tx_byte : in std_logic_vector(7 downto 0);
         i_byte_cnt : in std_logic_vector(7 downto 0);
---        i_addr : in std_logic_vector(6 downto 0);
-        o_buffer_clear : out std_logic; -- ( done = 1, active = 0 )
+        i_SDA : in std_logic;
         i_done_clear : in std_logic;
         i_buffer_clear : in std_logic; 
-        o_busy : out std_logic;
-        o_done : out std_logic;
+        o_buffer_clear : out std_logic := '0'; 
+        o_busy : out std_logic := '0';
+        o_done : out std_logic := '0';
         o_SCL : out std_logic;
-        i_SDA : in std_logic;
         o_SDA : out std_logic	
     );
 end entity;
@@ -47,7 +49,7 @@ end entity;
 
 architecture i2c_module_write_arch of i2c_module_write is
 
-    constant clock_divider_int : integer := integer(real(g_CLK_RATE) / real(10000));    -- Count needed to produce 100kHz I2C Clock 
+    constant clock_divider_int : integer := integer(real(g_CLK_RATE) / real(100000))/4;    -- Count needed to produce 100kHz I2C Clock 
     
 	-- Initializations
 	type state_type is (IDLE, START, START2, START3,
@@ -56,26 +58,25 @@ architecture i2c_module_write_arch of i2c_module_write is
 	                    DATA1, DATA2, DATA3, DATA4, DATA5, DATA6, DATA7, DATA8,
 	                    ACKN, STOP, STOP2, STOP3, WAIT_CLEAR, WAIT_BUF_CLEAR);
 	                    
-	signal current_state, next_state : state_type;
+	signal current_state : state_type := IDLE;
+	signal next_state : state_type := START;
 	signal halfcount_int : integer range 0 to 100000 := 0;
-	signal delay_count : integer range 0 to 20;
-	signal cnt : integer range 0 to 20;
+	signal delay_count : integer range 0 to 20 := 0;
+	signal cnt : integer range 0 to 20 := 0;
 	signal cycle_cnt : integer range 0 to 3 := 0;
 
 	signal s_BYTECNT_int : integer range 0 to 10000 := 2; 
-	signal s_SENTCNT_int : integer range 0 to 10000;
 	
 	signal s_SDA_int : std_logic := '1'; 
 	signal s_SDA_EN : std_logic;
 	signal s_SCL_EN : std_logic;
 	signal s_SCL_int : std_logic := '1';
 	
-	signal s_START_int : std_logic;
 	signal s_HALFSCL_int : std_logic := '0';
 	
 	-- TODO: RW_int values should come from port input
 	signal RW_int : std_logic := '0'; -- READ : 1 , WRITE : 0
-	signal i_addr : std_logic_vector(6 downto 0) := "1110000"; -- hard coded address 
+--	signal i_addr : std_logic_vector(6 downto 0) := "1110000"; -- hard coded address 
     signal addr_buf : unsigned(6 downto 0);
 	signal data_buf : unsigned(7 downto 0);
 	signal current_byte : std_logic_vector(7 downto 0);
@@ -85,19 +86,19 @@ architecture i2c_module_write_arch of i2c_module_write is
 	
 	--- TEMP 
 	signal state : std_logic := '1';
-    signal cnt2 : integer range 0 to 200000;
+    signal cnt2 : integer range 0 to 200000 := 0;
 	--- END TEMP
 
     begin
     
         total_bytes <= to_integer(unsigned(i_byte_cnt));
-    
+          
 	
 	PROC2 : process(i_CLK, i_reset_n)
       begin
         if(rising_edge(i_CLK)) then
             cnt2 <= cnt2 + 1;
-            if(cnt2 = (clock_divider_int-1) ) then
+            if(cnt2 >= (clock_divider_int-1) ) then
                 cnt2 <= 0;
                
                 s_HALFSCL_int <= not s_HALFSCL_int;
@@ -145,7 +146,11 @@ architecture i2c_module_write_arch of i2c_module_write is
                     when IDLE => 
                         if ((i_en = '1')) then 
                             next_state <= START;
+                        else 
+                            next_state <= IDLE;
                         end if;
+                        
+--                        next_state <= START;
                         
                     when START => 
                         next_state <= START2;
@@ -386,6 +391,7 @@ architecture i2c_module_write_arch of i2c_module_write is
 
      o_SCL   <= s_SCL_int when s_SCL_EN = '1' else 'Z';
      o_SDA   <= s_SDA_int when s_SDA_EN = '1' else 'Z';
+     
 
     
 end architecture;
